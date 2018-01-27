7aaa4544563cda44d78f407795348eb680cac227
9robin3 <andblom.robin@gmail.com>
2018-01-27 15:19:44 +0100

    New spaceship (with antenna) 1 +  title screen fixes

---
+++ Assets/Scripts/StartButton_Click.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class StartButton_Click : MonoBehaviour
+{
+    public GameObject startBtn;
+
+    public void StartClick()
+    {
+        EditorSceneManager.LoadScene("scene_mikkel");
+    }
+}
+
+
