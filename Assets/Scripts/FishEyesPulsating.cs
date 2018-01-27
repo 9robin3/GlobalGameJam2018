643d9d64e47849263cc8c6ab629303b197165b7e
Pal.Schakonat@gmail.com <g3pst9az0>
2018-01-27 13:54:49 +0100

    Camera
    Cool Camera forever

---
+++ Assets/Scripts/FishEyesPulsating.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityStandardAssets.ImageEffects {
+
+    public class FishEyesPulsating : MonoBehaviour {
+
+        [SerializeField]
+        AnimationCurve curve;
+        [SerializeField]
+        float curveTime;
+        [SerializeField]
+        float fishEyeStrenghtMultiplier;
+
+        Fisheye fisheyeComponent;
+        float timer;
+
+        private void Start()
+        {
+            fisheyeComponent = GetComponent<Fisheye>();
+        }
+
+        private void Update()
+        {
+            if (timer <= curveTime)
+            {
+                timer = timer + Time.deltaTime;
+            }
+            else
+            {
+                timer = 0;
+            }
+
+            fisheyeComponent.strengthX = (curve.Evaluate(timer) * fishEyeStrenghtMultiplier);
+            fisheyeComponent.strengthY = (curve.Evaluate(timer) * fishEyeStrenghtMultiplier);
+
+            print(curve.Evaluate(timer));
+        }
+
+    }
+}
